b0b8f4fb6f9dfae99fbd2b4aed4f6dc4d1426de9
Jakhongir Mirzakhmatov <94097414+Jakhongir-M@users.noreply.github.com>
2022-05-19 06:01:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let x = +prompt("birinchi sonni kiriting");
+let y = +prompt("ikkinchi sonni kiriting");
+let z = +prompt("uchinchi sonni kiriting");
+
+let result = x + y + z;
+alert(`${result} soni chiqdi, endi buni nechi songa bo'lmoqchisiz `);
+let ort = prompt("Bo'lmoqchi bolgan sonni kiriting");
+
+let mass = result / ort;
+alert(mass);
